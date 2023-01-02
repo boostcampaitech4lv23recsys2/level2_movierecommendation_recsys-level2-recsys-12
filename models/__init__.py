ae52199a20d98c348e3e67402b68109c6e58b8a9
JisooRyu99 <cussion09@gmail.com>
2023-01-02 18:03:08 +0900

    Feat : Add EASE Baseline code

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .Admm_SLIM import *
 from .Multi_DAE import *
 from .Multi_VAE import *
-from .S3Rec import *
-from .model import *
+# from .S3Rec import *
+# from .model import *
+from .EASE import *
---
+++ train/EASE_train.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.append("../")
+
+from models.EASE import EASE
+import pandas as pd
+import os
+from datetime import datetime
+from pytz import timezone
+from arg_parse.EASE_args import parse_args
+from data_loader.EASE_dataloader import dataloader
+from datetime import datetime
+#from trainer.EASE_trainer import predict
+
+import warnings
+warnings.filterwarnings(action="ignore")
+
+
+def main(args):
+    
+    train_df, users, items = dataloader(args)
+    
+    ease = EASE()
+    
+    ease.fit(train_df, args.lambda_)
+    #breakpoint()
+    result_df = ease.predict(train_df, users, items, args.K)
+    
+    print(result_df)
+
+
+    file_name = datetime.now(timezone('Asia/Seoul')).strftime('%Y%m%d_%H%M%S')
+    result_df[["user", "item"]].to_csv(args.output_dir + f"EASE_{file_name}_{args.K}.csv", index=False)
+
+   
+if __name__ == "__main__":
+    args = parse_args()
+    os.makedirs(args.output_dir, exist_ok=True)
+    main(args)
+    
